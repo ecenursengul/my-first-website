1a283c614ef28fc3644c7bdabb2fab6e466b01ad
ecenursengul <sengulecenur@gmail.com>
2026-03-31 16:03:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let deneme = 20;
+
+console.log(deneme);
+
+let x;
+
+console.log(x);
+
+let benim_kedim;
+
+console.log(benim_kedim);
+
+x = 30;
+
+benim_kedim = 50;
+
+const coding = 80;
+
+console.log(coding);
+
+let greeting = "Hello"
+
+let name = "Alice"
+
+let isStudent = true;
+
+let hasJob = false;
+
+let mario = "100";
+
+console.log(mario);
+
+let space = " "
+
+let h = greeting + space + name
+
+console.log (h);
